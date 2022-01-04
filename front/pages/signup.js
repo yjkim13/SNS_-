6b9886d24c19514338a4efb0fabdfe71f06361f5
yjkim13 <sk940913@naver.com>
2022-01-04 21:29:21 +0900

    page 설정 및 AppLayout

---
+++ front/pages/signup.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import AppLayout from '../components/AppLayout';
+
+const SignUp = () => {
+    return (
+        <AppLayout>
+            <div>
+                회원가입 페이지
+            </div>
+        </AppLayout>
+
+    )
+};
+
+export default SignUp
