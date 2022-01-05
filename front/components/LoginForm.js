ddf8c63b7bfc7f5a7bdd5c095b4d5f8db6c574a6
yjkim13 <sk940913@naver.com>
2022-01-05 17:33:32 +0900

    로그인 폼 만들기 및 리랜더링, 더미데이터로 로그인하기

---
+++ front/components/LoginForm.js
@@ -0,0 +1,60 @@
+import React, { useState, useCallback } from 'react'
+import { Button, Form, Input } from 'antd'
+import Link from 'next/link'
+import styled from 'styled-components'
+
+const ButtonWrapper = styled.div`
+margin-top: 10px
+`;
+
+const FormWrapper = styled(Form)`
+padding: 10px
+`;
+
+const LoginForm = ({ setIsLoggedIn }) => {
+    const [id, setId] = useState('')
+    const [password, setPassword] = useState('')
+
+    const onChangeId = useCallback((e) => {
+        setId(e.target.value)
+    }, []);
+
+    const onChangePassword = useCallback((e) => {
+        setPassword(e.target.value)
+    }, []);
+
+    const onSubmitForm = useCallback(() => {
+        console.log(id, password);
+        setIsLoggedIn(true);
+
+    }, [id, password])
+
+    return (
+        <FormWrapper onFinish={onSubmitForm}>
+            <div>
+                <label htmlFor='user-id'>아이디</label>
+                <br />
+                <Input name="user-id" value={id} onChange={onChangeId} required />
+            </div>
+            <div>
+                <label htmlFor='user-password'>비밀번호</label>
+                <br />
+                <Input
+                    name="user-password"
+                    type="password"
+                    value={password}
+                    onChange={onChangePassword}
+                    required
+                />
+            </div>
+            <ButtonWrapper>
+                <Button type='primary' htmlType='submit' loading={false}>로그인</Button>
+                <Link href="/signup"><a><button>회원가입</button></a></Link>
+            </ButtonWrapper>
+            <div></div>
+            <div></div>
+        </FormWrapper>
+    )
+}
+
+export default LoginForm
