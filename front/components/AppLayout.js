86f69ebea21b559f11c3c9265ac2215cf0683b30
yjkim13 <sk940913@naver.com>
2022-01-05 17:34:50 +0900

    리랜더링

--- front/components/AppLayout.js
+++ front/components/AppLayout.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types';
 import Link from 'next/link'
-import { Menu } from 'antd'
+import { Menu, Input, Row, Col } from 'antd'
+import styled from 'styled-components';
+
+import UserProfile from './UserProfile'
+import LoginForm from './LoginForm'
+
+const SearchInput = styled(Input.Search)`
+vertical-align: middle
+`;
 
 const AppLayout = ({ children }) => {
+
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
     return (
         <div>
             <Menu mode='horizontal'>
@@ -14,11 +25,23 @@ const AppLayout = ({ children }) => {
                     <Link href="/profile"><a>프로필</a></Link>
                 </Menu.Item>
                 <Menu.Item>
+                    <SearchInput />
+                </Menu.Item>
+                <Menu.Item>
                     <Link href="/signup"><a>회원가입</a></Link>
                 </Menu.Item>
             </Menu>
-
-            {children}
+            <Row gutter={8}>
+                <Col xs={24} md={6}>
+                    {isLoggedIn ? <UserProfile setIsLoggedIn={setIsLoggedIn} /> : <LoginForm setIsLoggedIn={setIsLoggedIn} />}
+                </Col>
+                <Col xs={24} md={12}>
+                    {children}
+                </Col>
+                <Col xs={24} md={6}>
+                    <a href='https://www.naver.com' target="_blank" rel='noreferrer noopener'>Naver</a>
+                </Col>
+            </Row>
         </div>
     )
 };
